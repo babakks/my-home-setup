a55c0c113a9aff67ec366efb00d56b69979b1079
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-01-14 22:02:52 +0330

    No dereference symlink
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- add-file.sh
+++ add-file.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
 script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-src_path="$(realpath "$1")"
+src_path="$(realpath --no-symlinks "$1")"
 dest_path="${script_path}/default${src_path}"
 dest_dir="$(dirname "$dest_path")"
 mkdir -p "$dest_dir"
-cp "$src_path" "$dest_path"
+cp --no-dereference "$src_path" "$dest_path"
